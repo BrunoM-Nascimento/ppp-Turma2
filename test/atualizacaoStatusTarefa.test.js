a05a3845ab6bfdd438da9a6afd036049dcb4b6bb
Bruno <brunomacena69@gmail.com>
2025-11-04 20:41:59 -0300

    adicionando teste de atualização de status das tarefas

---
+++ test/atualizacaoStatusTarefa.test.js
@@ -0,0 +1,58 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Atualização de status da tarefa - PATCH /tasks/{id}/status', () => {
+
+    // Token do admin 
+    const adminToken = 'TOKEN_DE_ADMIN';
+
+    // Token de usuário comum 
+    const userToken = 'TOKEN_DE_USUARIO_COMUM';
+
+    // ID da tarefa que você quer testar
+    const taskId = 1;
+
+    // Atualização do status da tarefa: "não realizado", "em andamento", "concluída".
+    it('Deve retornar 200 e atualizar o status da tarefa quando o usuario estiver autenticado', async () => {
+        const resposta = await request('http://localhost:3000')
+            .patch(`/tasks/${taskId}/status`)
+            .set('Authorization', `Bearer ${userToken}`)
+            .set('Content-Type', 'application/json')
+            .send({ status: 'concluída' });
+
+        console.log('Status:', resposta.status);
+        console.log('Body:', resposta.body);
+
+        expect(resposta.status).to.equal(200);
+        expect(resposta.body).to.have.property('status');
+    });
+
+    it('Deve retornar 401 se não enviar token', async () => {
+        const resposta = await request('http://localhost:3000')
+            .patch(`/tasks/${taskId}/status`)
+            .set('Content-Type', 'application/json')
+            .send({ status: 'concluída' });
+
+        expect(resposta.status).to.equal(401);
+    });
+
+    it('Deve retornar 403 acesso restrito a usuarios', async () => {
+        const resposta = await request('http://localhost:3000')
+            .patch(`/tasks/${taskId}/status`)
+            .set('Authorization', `Bearer ${adminToken}`)
+            .set('Content-Type', 'application/json')
+            .send({ status: 'concluída' });
+
+        expect(resposta.status).to.equal(403);
+    });
+
+    it('Deve retornar 404 se a tarefa não existir', async () => {
+        const resposta = await request('http://localhost:3000')
+            .patch('/tasks/999/status')
+            .set('Authorization', `Bearer ${userToken}`)
+            .set('Content-Type', 'application/json')
+            .send({ status: 'concluída' });
+
+        expect(resposta.status).to.equal(404);
+    });
+});
